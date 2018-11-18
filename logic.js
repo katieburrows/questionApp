b39a3062a749d8d4bb1d6e14308fbb7670f03fc9
katieburrows <katie_burrows@me.com>
2018-11-17 22:32:32 -0700

    added h1 to index, created question in .js file and set up counter variables

---
+++ logic.js
@@ -0,0 +1,30 @@
+//array of questions to display on the page.
+var questions = [
+    {
+        q: "There are 7 colors in the rainbow",
+        a: "t"
+    },
+    {
+        q: "There are 2 continents in the world",
+        a: "f",
+    },
+    {
+        q: "The capitol of Colorado is Denver",
+        a: "t",
+    },
+    {
+        q: "Traffic on I25 is light",
+        a: "f",
+    },
+    {
+        q: "Alexander Hamilton was the 2nd U.S. President",
+        a: "f",
+    }  
+]
+
+//counter variables for questions
+var correctAnswer = 0;
+var wrongAnswer = 0;
+
+
+
